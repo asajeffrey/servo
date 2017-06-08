ff058b1c81983aaa7d20b5ea8c6db643bb241d66
Alan Jeffrey <ajeffrey@mozilla.com>
2017-06-08 11:00:18 -0500

    WIP

--- components/script/dom/paintworkletglobalscope.rs
+++ components/script/dom/paintworkletglobalscope.rs
@@ -19,10 +19,17 @@ use dom::workletglobalscope::WorkletGlobalScopeInit;
 use dom_struct::dom_struct;
 use euclid::Size2D;
 use ipc_channel::ipc::IpcSharedMemory;
+use js::jsapi::Call;
+use js::jsapi::Construct1;
+use js::jsapi::HandleValueArray;
 use js::jsapi::Heap;
 use js::jsapi::IsCallable;
 use js::jsapi::IsConstructor;
+use js::jsapi::JS_ClearPendingException;
+use js::jsapi::JS_IsExceptionPending;
 use js::jsval::JSVal;
+use js::jsval::ObjectValue;
+use js::jsval::UndefinedValue;
 use js::rust::Runtime;
 use msg::constellation_msg::PipelineId;
 use net_traits::image::base::Image;
@@ -30,7 +37,10 @@ use net_traits::image::base::PixelFormat;
 use script_traits::PaintWorkletError;
 use servo_atoms::Atom;
 use servo_url::ServoUrl;
+use std::cell::Cell;
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::ptr::null_mut;
 use std::rc::Rc;
 use std::sync::mpsc::Sender;
 
@@ -40,8 +50,10 @@ use std::sync::mpsc::Sender;
 pub struct PaintWorkletGlobalScope {
     /// The worklet global for this object
     worklet_global: WorkletGlobalScope,
-    /// The registered paint definitions
+    /// https://drafts.css-houdini.org/css-paint-api/#paint-definitions
     paint_definitions: DOMRefCell<HashMap<Atom, PaintDefinition>>,
+    /// https://drafts.css-houdini.org/css-paint-api/#paint-class-instances
+    paint_class_instances: DOMRefCell<HashMap<Atom, Heap<JSVal>>>,
     /// A buffer to draw into
     buffer: DOMRefCell<Vec<u8>>,
 }
@@ -56,7 +68,8 @@ impl PaintWorkletGlobalScope {
         debug!("Creating paint worklet global scope for pipeline {}.", pipeline_id);
         let global = box PaintWorkletGlobalScope {
             worklet_global: WorkletGlobalScope::new_inherited(pipeline_id, base_url, init),
-            paint_definitions: DOMRefCell::new(HashMap::new()),
+            paint_definitions: Default::default(),
+            paint_class_instances: Default::default(),
             buffer: Default::default(),
         };
         unsafe { PaintWorkletGlobalScopeBinding::Wrap(runtime.cx(), global) }
@@ -79,19 +92,21 @@ impl PaintWorkletGlobalScope {
     }
 
     /// https://drafts.css-houdini.org/css-paint-api/#invoke-a-paint-callback
+    #[allow(unsafe_code)]
     fn invoke_a_paint_callback(&self,
                                name: Atom,
                                size: Size2D<Au>,
                                sender: Sender<Result<Image, PaintWorkletError>>)
     {
+        let cx = self.worklet_global.get_cx();
         let width = size.width.to_px().abs() as u32;
         let height = size.height.to_px().abs() as u32;
         debug!("Invoking a paint callback {}({},{}).", name, width, height);
 
         // TODO: Steps 1-2.1.
         // Step 3.
-        let definition = match self.paint_definitions.borrow().get(&name).cloned() {
-            Some(defn) => defn,
+        let definition = match self.paint_definitions.borrow().get(&name) {
+            Some(defn) => defn.clone(),
             None => {
                 // Step 2.2.
                 warn!("Drawing un-registered paint definition {}.", name);
@@ -101,11 +116,54 @@ impl PaintWorkletGlobalScope {
             }
         };
 
-        // TODO: Steps 4-12
+        // Steps 4-5
+        rooted!(in(cx) let mut paint_instance = UndefinedValue());
+        match self.paint_class_instances.borrow_mut().entry(name.clone()) {
+            Entry::Occupied(entry) => paint_instance.set(entry.get().get()),
+            Entry::Vacant(entry) => {
+                // Step 5.1
+                if !definition.constructor_valid_flag.get() {
+                    debug!("Drawing invalid paint definition {}.", name);
+                    let image = self.placeholder_image(width, height, [0x00, 0x00, 0xFF, 0xFF]);
+                    let _ = sender.send(Ok(image));
+                    return;
+                }
+                // Step 5.2-5.3
+                let args = HandleValueArray::new();
+                rooted!(in(cx) let mut result = null_mut());
+                unsafe { Construct1(cx, definition.class_constructor.handle(), &args, result.handle_mut()); }
+                paint_instance.set(ObjectValue(result.get()));
+                if unsafe { JS_IsExceptionPending(cx) } {
+                    debug!("Paint constructor threw an exception {}.", name);
+                    unsafe { JS_ClearPendingException(cx); }
+                    definition.constructor_valid_flag.set(false);
+                    let image = self.placeholder_image(width, height, [0x00, 0x00, 0xFF, 0xFF]);
+                    let _ = sender.send(Ok(image));
+                    return;
+                }
+                // Step 5.4
+                entry.insert(Heap::new(paint_instance.get()));
+            }
+        };
+
+        // TODO: Steps 6-10
+        // Steps 11-12
+        debug!("Invoking paint function {}.", name);
+        let args = HandleValueArray::new();
+        rooted!(in(cx) let mut result = UndefinedValue());
+        unsafe { Call(cx, paint_instance.handle(), definition.paint_function.handle(), &args, result.handle_mut()); }
+
+        // Step 13.
+        if unsafe { JS_IsExceptionPending(cx) } {
+            debug!("Paint function threw an exception {}.", name);
+            unsafe { JS_ClearPendingException(cx); }
+            let image = self.placeholder_image(width, height, [0x00, 0x00, 0xFF, 0xFF]);
+            let _ = sender.send(Ok(image));
+            return;
+        }
+
         // For now, we just build a dummy image.
         let image = self.placeholder_image(width, height, [0xFF, 0x00, 0x00, 0xFF]);
-
-        // Step 13.                             
         let _ = sender.send(Ok(image));
     }
 
@@ -131,10 +189,10 @@ impl PaintWorkletGlobalScope {
 impl PaintWorkletGlobalScopeMethods for PaintWorkletGlobalScope {
     #[allow(unsafe_code)]
     /// https://drafts.css-houdini.org/css-paint-api/#dom-paintworkletglobalscope-registerpaint
-    fn RegisterPaint(&self, name: DOMString, paintCtor: Rc<VoidFunction>) -> Fallible<()> {
+    fn RegisterPaint(&self, name: DOMString, paint_ctor: Rc<VoidFunction>) -> Fallible<()> {
         let name = Atom::from(name);
         let cx = self.worklet_global.get_cx();
-        rooted!(in(cx) let paint_obj = paintCtor.callback_holder().get());
+        rooted!(in(cx) let paint_obj = paint_ctor.callback_holder().get());
 
         debug!("Registering paint image name {}.", name);
 
@@ -198,10 +256,10 @@ impl PaintWorkletGlobalScopeMethods for PaintWorkletGlobalScope {
 
         // Step 19.
         let definition = PaintDefinition {
-            class_constructor: paintCtor,
+            class_constructor: Heap::new(ObjectValue(paint_obj.get())),
             paint_function: Heap::new(paint_function),
-            constructor_valid_flag: true,
-            input_properties: Rc::new(input_properties),
+            constructor_valid_flag: Rc::new(Cell::new(true)),
+            input_properties: input_properties,
             context_alpha_flag: alpha,
         };
 
@@ -224,11 +282,10 @@ pub enum PaintWorkletTask {
 /// https://drafts.css-houdini.org/css-paint-api/#paint-definition
 #[derive(Clone, JSTraceable, HeapSizeOf)]
 struct PaintDefinition {
-    #[ignore_heap_size_of = "Rc"]
-    class_constructor: Rc<VoidFunction>,
+    class_constructor: Heap<JSVal>,
     paint_function: Heap<JSVal>,
-    constructor_valid_flag: bool,
     #[ignore_heap_size_of = "Rc"]
-    input_properties: Rc<Vec<DOMString>>,
+    constructor_valid_flag: Rc<Cell<bool>>,
+    input_properties: Vec<DOMString>,
     context_alpha_flag: bool,
 }
